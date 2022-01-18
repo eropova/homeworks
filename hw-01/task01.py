70a0c104121d8d10fbc88552f4f75fc76d71326b
Tatyana Eropova <Tatyana_Eropova@epam.com>
2022-01-18 12:28:47 +0300

    corrections 2

--- hw-01/task01.py
+++ hw-01/task01.py
@@ -1,11 +1,11 @@
+x = int(input('Ввод:\n'))
+y = int(input())
+
 def math_programm():
     """Программа принимает два числа
      и возвращает сумму, произведение и степень этих двух чисел"""
 
-    x = int(input('Ввод:\n'))
-    y = int(input())
-
     return f'Вывод:\n{x + y} {x * y} {x ** y}'
 
 
-print(math_programm())
+print(math_programm(x, y))
