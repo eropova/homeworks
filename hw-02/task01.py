9b0d1422064b302d3e8aed0892aab4b3866db111
eropova <97430160+eropova@users.noreply.github.com>
2022-01-23 16:55:29 +0300

    Update task01.py
    task01 list заменен на set, зачисление стало методом, скил передаются как группой так и по одному

--- hw-02/task01.py
+++ hw-02/task01.py
@@ -7,25 +7,31 @@ class Student:
         self.name = name
         self.surname = surname
         self.age = age
-        self.skills = ['English']
-        self.is_learning = True
+        self.skills = {'English'}
+        self.learning()
 
     def learning(self):
-        """Метод обучения: в атрибут self.skills добавляются значения SQL, Linux и Python)"""
+        """Метод зачисления на курс"""
+        self.is_learning = True
 
-        subjects = ['SQL', 'Linux', 'Python']
-        for sub in subjects:
-            self.skills.append(sub)
+    def new_knowledge(self, subject):
+        """Метод обучения: в атрибут self.skills добавляются значения освоенных курсов"""
+        if type(subject) != str:
+            for sub in subject:
+                self.skills.add(sub)
+        else:
+            self.skills.add(subject)
 
     def accept(self):
         """Метод прием на работу: если студент обладает всеми необходимыми навыками
         возвращает "успешное сообщение", иначе - "неуспешное"."""
 
-        skills_need = ['English', 'SQL', 'Linux', 'Python']
+        skills_need = {'English', 'SQL', 'Linux', 'Python'}
         if self.skills == skills_need:
             return f"{self.name}, you're accepted! Good job!"
         else:
             return f"{self.name}, sorry, you're denied! Try next time."
+
 
 students = []
 
@@ -34,10 +40,13 @@ Bob = Student('Bob', 'Brown', 23)
 Cean = Student('Cean', 'Crown', 19)
 students.extend([Alice, Bob, Cean])
 
-for student in students:
-    student.learning()
-Cean.skills.remove('Python')
+Alice.new_knowledge('SQL')
+Alice.new_knowledge('Linux')
+Alice.new_knowledge('Python')
+
+Bob.new_knowledge(('SQL', 'Linux', 'Python'))
+
+Cean.new_knowledge(['SQL', 'Linux'])
 
 for student in students:
     print(student.accept())
-
