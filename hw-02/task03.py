032bf1cb85616218dd5fe7ac6682f68ea86aacd6
eropova <97430160+eropova@users.noreply.github.com>
2022-01-23 16:59:51 +0300

    Update task03.py
    метод go перенесен в класс лыжника, изменен класс Moving, в main только создание экземпляра лыжника и вызов метода спуска

--- hw-02/task03.py
+++ hw-02/task03.py
@@ -3,57 +3,38 @@ from random import choice
 
 class Skier:
     """Класс для описания свойств лыжника:
-    name - имя, age возраст, on_track - позиция на трассе, по умолчанию False"""
+    name - имя, age возраст"""
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        self.on_track = False
 
-    def get_ready(self):
-        """Метод перемещает лыжника на стартовую позицию для спуска"""
-        self.on_track = True
-
-    def set_moving(self, moving):
-        """Метод для связи объекта с объектом класса Moving"""
-        self.moving = moving
+    def go(self, time):
+        """Метод для для спуска лыжника"""
+        time = Moving.time
+        for _i in range(time):
+            gals = choice(list(Moving.speed.keys()))
+            Moving.distance_pass += Moving.speed[gals]
+            Moving.directions.append(gals)
+        return (f"За {Moving.time} секунд лыжник {self.name} проехал {Moving.distance_pass} метров "
+                f"по следующим направлениям:\n" + "\n".join(Moving.directions))
 
 
 class Moving:
     """Класс для описания свойств спуска:
     speed - скорости различных направлений спуска,
     distance_pass - пройденная дистанция,
-    directions - список пройденных направлений"""
+    directions - список пройденных направлений,
+    time - время спуска"""
     speed = {'east': 1, 'south': 5, 'west': 1}
     distance_pass = 0
     directions = []
-
-    def __init__(self, skier, time):
-        self.skier = skier
-        skier.set_moving(self)
-        self.time = time
-
-    def go(self):
-        """Метод для для спуска лыжника"""
-        for i in range(self.time):
-            gals = choice(list(Moving.speed.keys()))
-            Moving.distance_pass += Moving.speed[gals]
-            Moving.directions.append(gals)
-        return (f"За {self.time} секунд лыжник {self.skier.name} проехал {Moving.distance_pass} метров "
-               f"по следующим направлениям:\n" + "\n".join(Moving.directions))
+    time = 17
 
 
 def main():
-    person_name = input('Напишите имя: \n')
-    person_age = int(input('Напишите возраст: \n'))
-    person_time = int(input('Напишите время спуска: \n'))
-    skier1 = Skier(person_name, person_age)
-    skier1.get_ready()
-    #skier1.get_ready()
-    moving1 = Moving(skier1, person_time)
-    if skier1.on_track:
-        print(moving1.go())
-    else:
-        print('для спуска поднимитесь на гору')
+    skier1 = Skier('John', 99)
+    print(skier1.go(Moving()))
+
 
 if __name__ == "__main__":
     main()
