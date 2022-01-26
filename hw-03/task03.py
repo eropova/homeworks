c97e72bd0ecb913443003ae574634d8f0e327855
eropova <97430160+eropova@users.noreply.github.com>
2022-01-26 15:53:20 +0300

    Update task03.py
    Убрана функция def polynom().

--- hw-03/task03.py
+++ hw-03/task03.py
@@ -1,4 +1,5 @@
 import argparse
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-a', type=float, nargs='+', action='store')
 parser.add_argument('-x', type=float, nargs='?', action='store')
@@ -6,27 +7,17 @@ parser.add_argument('-v', '--verbose', action='store_true')
 
 poly = parser.parse_args()
 
+answer = 0
+n = len(poly.a)
+out_st = []
 
-def polynom():
-    coef = poly.a
-    x = poly.x
-    v = poly.verbose
-    answer = 0
-    n = len(coef)
-    out_st = []
+for i in range(n):
+    answer += (poly.a[i] * poly.x**(n-1))
+    out_st.append([poly.a[i], poly.x**(n-1)])
+    n -= 1
 
-    for _ in range(n):
-        answer += (coef[_] * x**(n-1))
-        out_st.append([coef[_], x**(n-1)])
-        n -= 1
-    #print(out_st)
-    #print(out_st[0][0], out_st[0][1])
+if poly.verbose:
+    print("+".join(f"{out_st[a][0]}*{out_st[a][1]}" for a in range(len(poly.a))) + f"={answer}")
 
-    if v:
-        return "+".join(f"{out_st[a][0]}*{out_st[a][1]}" for a in range(len(coef))) + f"={answer}"
-
-    else:
-        return answer
-
-
-print(polynom())
+else:
+    print(answer)
